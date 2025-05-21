9f1841e84fd1025090f62a878227f797adce624d
Achuthanantha N J <achutha407@gmail.com>
2025-05-21 14:57:05 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+ini_set('memory_limit', '1024M');
+ini_set('max_execution_time', 30);
+
+$host = "localhost";
+$username = "root"; 
+$password = "";
+$dbname = "college_management";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die(json_encode(["status" => "error", "message" => "Database Connection Failed: " . $conn->connect_error]));
+}
+?>
 
